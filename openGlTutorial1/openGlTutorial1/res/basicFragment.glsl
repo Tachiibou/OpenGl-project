5a1cdf3c40ff1d4fe654189ca979326cc013ee6c
Elias <elias.frank.@gmail.com>
2016-04-09 19:14:11 +0200

    BasicFragment variable
    Gave basicFragment an out color variable instead of gl_fragColor. It now
    uses this so we can out to our own framebuffer

--- openGlTutorial1/openGlTutorial1/res/basicFragment.glsl
+++ openGlTutorial1/openGlTutorial1/res/basicFragment.glsl
@@ -1,10 +1,11 @@
 #version 430
 
 uniform sampler2D texture;
+out color;
 
 in vec2 fragment_uv;
 in vec3 fragment_normal;
 
 void main(){
-	gl_FragColor = texture2D(texture, fragment_uv);
+	color = texture2D(texture, fragment_uv);
 }
